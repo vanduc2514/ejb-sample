0712e258639171474f7364a6f0f8a8e67e90070a
Nguyen Van Duc <duc03.nguyenvan@ifisolution.com>
2020-10-07 07:25:26 +0300

    add simple lookup remote bean method

--- src/main/java/com/ifi/client/RemoteEJBClient.java
+++ src/main/java/com/ifi/client/RemoteEJBClient.java
@@ -1,7 +1,30 @@
 package com.ifi.client;
 
-public class RemoteEJBClient {
-    public static void main(String[] args) {
+import com.ifi.stateless.HelloWorld;
 
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import java.util.Hashtable;
+
+public class RemoteEJBClient {
+    public static void main(String[] args) throws NamingException {
+        invokeStatelessBean();
+    }
+
+    private static void invokeStatelessBean() throws NamingException {
+        final HelloWorld helloWorldBean = lookupBean();
+        if (helloWorldBean != null) {
+            System.out.println(helloWorldBean.sayHello());
+        } else {
+            System.out.println("Bean not found!");
+        }
+    }
+
+    private static HelloWorld lookupBean() throws NamingException {
+        final Hashtable<String, String> jndiProperties = new Hashtable<>();
+        jndiProperties.put(Context.URL_PKG_PREFIXES, "org.jboss.ejb.client.naming");
+        final Context context = new InitialContext(jndiProperties);
+        return (HelloWorld) context.lookup("ejb:/ejb-sample-1.0/HelloWorld!com.ifi.stateless.HelloWorld");
     }
 }
