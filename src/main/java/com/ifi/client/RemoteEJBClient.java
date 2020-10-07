b3a90123d10b01fa5d0fb6872239b946cac859d5
Nguyen Van Duc <duc03.nguyenvan@ifisolution.com>
2020-10-07 07:40:26 +0300

    Extract build properties method

--- src/main/java/com/ifi/client/RemoteEJBClient.java
+++ src/main/java/com/ifi/client/RemoteEJBClient.java
@@ -23,12 +23,17 @@ public class RemoteEJBClient {
     }
 
     private static HelloWorld lookupBean() throws NamingException {
+        final Properties jndiProperties = buildJNDIProperties();
+        final Context context = new InitialContext(jndiProperties);
+        return (HelloWorld) context.lookup("ejb:/ejb-sample-1.0/HelloWorld!com.ifi.stateless.HelloWorld");
+    }
+
+    private static Properties buildJNDIProperties() {
         final Properties jndiProperties = new Properties();
         jndiProperties.put(Context.URL_PKG_PREFIXES, "org.jboss.ejb.client.naming");
         jndiProperties.put(Context.INITIAL_CONTEXT_FACTORY, "org.jboss.naming.remote.client.InitialContextFactory");
         jndiProperties.put(Context.PROVIDER_URL, "remote://localhost:8080");
         jndiProperties.put("jboss.naming.client.ejb.context", true);
-        final Context context = new InitialContext(jndiProperties);
-        return (HelloWorld) context.lookup("ejb:/ejb-sample-1.0/HelloWorld!com.ifi.stateless.HelloWorld");
+        return jndiProperties;
     }
 }
