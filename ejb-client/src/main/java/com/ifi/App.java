1368494cebebc887a3697dce1451f9d9032b2e4d
Nguyen Van Duc <duc03.nguyenvan@ifisolution.com>
2020-10-07 13:42:33 +0300

    migrate from old removed client

--- ejb-client/src/main/java/com/ifi/App.java
+++ ejb-client/src/main/java/com/ifi/App.java
@@ -1,13 +1,41 @@
 package com.ifi;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import com.ifi.stateless.HelloWorld;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import java.util.Properties;
+
+public class App {
+    public static void main(String[] args) {
+        try {
+            invokeRemoteBean();
+        } catch (NamingException e) {
+            System.out.println("Bean name not found");
+        }
+    }
+
+    private static void invokeRemoteBean() throws NamingException {
+        final HelloWorld helloWorldBean = lookupBean();
+        if (helloWorldBean == null) {
+            return;
+        }
+        System.out.println(helloWorldBean.sayHello());
+    }
+
+    private static HelloWorld lookupBean() throws NamingException {
+        final Properties jndiProperties = buildJNDIProperties();
+        final Context context = new InitialContext(jndiProperties);
+        return (HelloWorld) context.lookup("ejb:/ejb-sample-1.0/HelloWorld!com.ifi.stateless.HelloWorld");
+    }
+
+    private static Properties buildJNDIProperties() {
+        final Properties jndiProperties = new Properties();
+        jndiProperties.put(Context.URL_PKG_PREFIXES, "org.jboss.ejb.client.naming");
+        jndiProperties.put(Context.INITIAL_CONTEXT_FACTORY, "org.jboss.naming.remote.client.InitialContextFactory");
+        jndiProperties.put(Context.PROVIDER_URL, "http-remoting://localhost:8080");
+        jndiProperties.put("jboss.naming.client.ejb.context", true);
+        return jndiProperties;
     }
 }
